d1dc65ba123add3dbad51172eebc95b9b2b7f301
josefranf <josefranf@gmail.com>
2026-07-15 17:30:45 +0200

    Update database host and password in setup.php

--- libs/setup.php
+++ libs/setup.php
@@ -15,10 +15,11 @@ $config->set('viewsFolder', 'views/');
 //$config->set('dbpass', '');
 
 
-$config->set('dbhost', 'shuttle.proxy.rlwy.net:49542');
+$config->set('dbhost', 'hayabusa.proxy.rlwy.net:55164');
 $config->set('dbname', 'railway');
 $config->set('dbuser', 'root');
-$config->set('dbpass', 'ciTQiihTMkrqrUVDoFPHleNoZEhOYzMi');
+$config->set('dbpass', 'HLATxBNBbujWqwhYVJzaLiRBIFZVpghh');
 
 //mysql://root:ciTQiihTMkrqrUVDoFPHleNoZEhOYzMi@shuttle.proxy.rlwy.net:49542/railway
+//mysql://root:HLATxBNBbujWqwhYVJzaLiRBIFZVpghh@hayabusa.proxy.rlwy.net:55164/railway  este es el mio
 ?>
